ddede58e02eaecf7755881b4b73f0e7ed14f1314
筱彤 赵 <275451183@qq.com>
2019-08-25 15:31:28 +0800

    add mirgration project

---
+++ Infrastructure.Repository/Migrations/20190825072357_changeTable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Repository.Migrations
+{
+    public partial class changeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreatedOn = table.Column<DateTime>(nullable: true),
+                    LastModifyOn = table.Column<DateTime>(nullable: true),
+                    MenuName = table.Column<string>(nullable: true),
+                    Test = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "childmenus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreatedOn = table.Column<DateTime>(nullable: true),
+                    LastModifyOn = table.Column<DateTime>(nullable: true),
+                    ChildMenuName = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    MenuId = table.Column<int>(nullable: false),
+                    ViewId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_childmenus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_childmenus_menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_childmenus_MenuId",
+                table: "childmenus",
+                column: "MenuId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "childmenus");
+
+            migrationBuilder.DropTable(
+                name: "menus");
+        }
+    }
+}
--- Infrastructure.Repository/RepositoryImplement/EFContext.cs
+++ Infrastructure.Repository/RepositoryImplement/EFContext.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Repository.RepositoryImplement
     {
         public EFContext(DbContextOptions<EFContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            //Database.EnsureCreated();
         }
         public virtual DbSet<Menu> menus { get; set; }
         public virtual DbSet<ChildMenu> childmenus { get; set; }
@@ -18,17 +18,6 @@ namespace Infrastructure.Repository.RepositoryImplement
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLazyLoadingProxies();
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Menu>().HasData(
-                new Menu() { Id = 1, MenuName = "Contract Management", CreatedOn = DateTime.Now },
-                new Menu() { Id = 2, MenuName = "Component Management", CreatedOn = DateTime.Now });
-
-            modelBuilder.Entity<ChildMenu>().HasData(
-                new ChildMenu() { Id = 1, MenuId = 1, ViewId = "div-Contract-VersionControl", Url= "/ContractInformationManage/VersionControl", ChildMenuName = "Version Control Management", CreatedOn = DateTime.Now },
-                new ChildMenu() { Id = 2, MenuId = 2, ViewId = "div-Component-ReviewCompoentManagement", Url = "/ComponentManageMent/ReviewComponentManagement", ChildMenuName = "Test Function", CreatedOn = DateTime.Now });
         }
     }
 }
