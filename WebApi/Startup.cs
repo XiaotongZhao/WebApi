5793ccea224c9aa0cab05ccc750c58542b53bbc9
筱彤 赵 <275451183@qq.com>
2019-12-03 13:03:52 +0800

    delete useless code

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -6,11 +6,9 @@ using Infrastructure.IoC.MapperConfig;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using WebApi.FilterAttribute;
 
 namespace WebApi
 {
